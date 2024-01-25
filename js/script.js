a1ba401ab0b676fa29c06d56d2fc5f7e43662250
Edveika <edvinasbureika@gmail.com>
2024-01-25 19:24:25 +0200

    Mobile navigation functionality

--- js/script.js
+++ js/script.js
@@ -7,9 +7,18 @@
 //   h1.style.backgroundColor = "red";
 // });
 
+/* Set current year */
 const yearEl = document.querySelector(".year");
 const currYear = new Date().getFullYear();
 yearEl.textContent = currYear;
+
+/* Mobile navigation */
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".main-header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
